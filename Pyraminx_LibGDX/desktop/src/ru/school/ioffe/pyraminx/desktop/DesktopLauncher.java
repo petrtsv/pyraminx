a97544604a3d9f7f90b3626352a6478855462b61
Petr Tsvetkov <petr.tsv@gmail.com>
2018-09-22 14:11:09 +0300

    LibGDX project created

---
+++ Pyraminx_LibGDX/desktop/src/ru/school/ioffe/pyraminx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.school.ioffe.pyraminx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.school.ioffe.pyraminx.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Application(), config);
+	}
+}
